b5a18a33373c31c14c28523e5f2d3c66bd72f97c
alex <82418767002qq.com>
2019-06-16 17:04:08 +0800

    rabbitmq 限流及死信队列

---
+++ src/main/java/com/yl/middleware/rabbitmq/advance/RateLimitConsumer.java
@@ -0,0 +1,62 @@
+package com.yl.middleware.rabbitmq.advance;
+
+import com.rabbitmq.client.*;
+import com.yl.middleware.rabbitmq.ConnectionFactory;
+
+import java.io.IOException;
+
+/**
+ * @author yu.alex
+ * @date 2019/6/16 11:00
+ * @des Rabbitmq消费端限流
+ */
+public class RateLimitConsumer extends DefaultConsumer {
+
+
+    public RateLimitConsumer(Channel channel) {
+        super(channel);
+    }
+
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+
+        System.err.println("收到消息");
+        System.err.println("consumerTag:" + consumerTag);
+        System.err.println("envelope:" + envelope);
+        System.err.println("properties:" + properties);
+        System.err.println("body:" + new String(body));
+        System.err.println("----------------------");
+
+        this.getChannel().basicAck(envelope.getDeliveryTag(), false);
+
+        //收到NACK表示消息签收失败,参数:消息表示、是否批量、是否重回队列(即将消息重新发送回队列,重回的消息放在队列的最尾端)
+        //this.getChannel().basicNack(envelope.getDeliveryTag(), false, true);
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        Channel channel = connection.createChannel();
+
+        String exchangeName = "dev_direct_exchange";
+        String routingKey = "confirm.save";
+        String exchangeType = "direct";
+        String queueName = "confirm.rateLimit";
+
+        channel.exchangeDeclare(exchangeName,exchangeType,true,false,null);
+        AMQP.Queue.DeclareOk queue = channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName,exchangeName,routingKey);
+
+        /**
+         * 限流设置
+         *  1.设置autoAck为false,消费消息之后手动确认
+         *  2.设置消息接收规则 参数:拉取消息大小(0表示不限制)、拉取消息个数、是否应用到channel级别,false表示应用到消费者级别
+         */
+        channel.basicQos(0,1,false);
+        channel.basicConsume(queueName,false,new RateLimitConsumer(channel));
+
+
+    }
+
+}
---
+++ src/main/java/com/yl/middleware/rabbitmq/advance/RateLimitProducer.java
@@ -0,0 +1,71 @@
+package com.yl.middleware.rabbitmq.advance;
+
+import com.rabbitmq.client.*;
+import com.yl.middleware.rabbitmq.ConnectionFactory;
+import sun.applet.Main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author yu.alex
+ * @date 2019/6/16 11:15
+ * @des
+ */
+public class RateLimitProducer {
+
+    public static void main(String[] args) throws Exception{
+
+        // 获取连接
+        Connection connection = ConnectionFactory.getConnection();
+
+        // 创建channel
+        Channel channel = connection.createChannel();
+
+        // 指定消息投递模式:消息确认模式
+        channel.confirmSelect();
+        // 添加确认监听,用于监听消息是否送达到exchange
+        channel.addConfirmListener(new ConfirmListener() {
+            // 确认成功 参数:1-唯一消息标签
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                System.err.println("消息ACK:" + deliveryTag);
+            }
+
+            // 确认失败
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                System.err.println("ConfirmListener NO ACK:" + deliveryTag);
+            }
+        });
+
+        /**
+         * 不可达消息监听,用于监听消息是否送达队列,比如路由键找不到队列等
+         * 注意:
+         *      1.exchange不存在会报错
+         */
+        channel.addReturnListener(new ReturnListener() {
+            @Override
+            public void handleReturn(int replyCode, String replyText, String exchange, String routingKey, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.err.println("ReturnListener Return err:" + replyCode);
+            }
+        });
+
+        String exchangeName = "dev_direct_exchange";
+        String routingKey = "confirm.save";
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = null;
+        while (!(line = scanner.nextLine()).equals("exit")){
+            /**
+             * 当使用ReturnListener的时候,mandatory必须设置为true
+             * 不然Broker端会自动删除不可达的消息
+             */
+            channel.basicPublish(exchangeName, routingKey,true, null, line.getBytes());
+        }
+        connection.close();
+
+    }
+
+}
